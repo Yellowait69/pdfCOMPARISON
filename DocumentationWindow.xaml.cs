25421d914d51758b0abf2606e8f43b1f1497d00e
maxen <etu52843@henallux.com>
2026-05-05 11:52:05 +0200

    modif

---
+++ DocumentationWindow.xaml.cs
@@ -0,0 +1,13 @@
+using System.Windows;
+
+namespace PDFComparison.Views
+{
+    public partial class DocumentationWindow : Window
+    {
+        public DocumentationWindow()
+        {
+            // C'est la ligne magique qui dessine ton écran !
+            InitializeComponent();
+        }
+    }
+}
